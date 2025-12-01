b1001cfa4395890cbf776de17d0efbe8d5de8ebe
Rox <roxseeley@gmail.com>
2025-12-01 15:41:59 -0700

    Day 1 part 1 complete

--- day1/rotate.py
+++ day1/rotate.py
@@ -1,5 +1,22 @@
-# create a list 0-99
 # go through file line by line, adjust the index accordingly
 # R= +index L= -index
 # every time index == 0, add to count
-# print countS
+# print counts
+
+def readPassword():
+    index = 50
+    count = 0
+    input = open("day1/input1.txt")
+    for line in input:
+        line = line.strip()
+        val = int(line[1:])
+        direction = line[0]
+        for i in range(val):
+            if direction == "R": index +=1
+            else: index -=1
+            if index > 99: index = 0
+            if index < 0: index = 99
+        if index == 0: count +=1
+    print(count)
+
+readPassword()
